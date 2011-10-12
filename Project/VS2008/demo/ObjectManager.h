7ab451290fdf398ae733e9b8b6c618c1095c6cea
ntin90@gmail.com <ntin90@gmail.com@515b9638-c7ee-b3bb-69fa-1e28f7a59f5f>
2011-10-12 04:49:44 +0000

    + object manager

---
+++ Project/VS2008/demo/ObjectManager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "TowerObject.h"
+#include "EnemyObject.h"
+#include "ObstacleObject.h"
+#include "BulletObject.h"
+#include "MapObject.h"
+
+class CObjectManager
+{
+public:
+	CObjectManager(void);
+	~CObjectManager(void);
+
+	void Init();
+	void Update();
+	void Render();
+	void Destroy();
+
+	void AddObject(CTowerObject*);
+	void AddObject(CEnemyObject*);
+	void AddObject(CObstacleObject*);
+	void AddObject(CBulletObject*);
+	void SetMapObject(CMapObject*);
+
+	CMapObject* Map;
+	CLutI<CTowerObject*> TowerList;
+	int TowerKeyCounter;
+	CLutI<CEnemyObject*> EnemyList;
+	int EnemyKeyCounter;
+	CLutI<CObstacleObject*> ObstacleList;
+	int ObstacleKeyCounter;
+	CLutI<CBulletObject*> BulletList;
+	int BulletKeyCounter;
+};
