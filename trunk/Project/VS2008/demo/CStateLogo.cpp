171d377217e7363bd1a4d799029ea4d41ccd61a4
hlv.nguyentrinh@gmail.com <hlv.nguyentrinh@gmail.com@515b9638-c7ee-b3bb-69fa-1e28f7a59f5f>
2011-11-12 04:12:32 +0000

    + Add TaskList + Add State Logo

---
+++ trunk/Project/VS2008/demo/CStateLogo.cpp
@@ -0,0 +1,32 @@
+#include "CStateLogo.h"
+
+
+CStateLogo::CStateLogo(void)
+{
+}
+
+
+CStateLogo::~CStateLogo(void)
+{
+}
+
+
+void CStateLogo::Init(){
+
+}
+
+void CStateLogo::OnControllerEvent(SControllerEvent Event){
+
+}
+
+void CStateLogo::Update(){
+
+}
+
+void CStateLogo::Render(){
+
+}
+
+void CStateLogo::Exit(){
+
+}
