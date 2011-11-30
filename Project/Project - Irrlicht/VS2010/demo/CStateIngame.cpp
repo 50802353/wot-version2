33b114d0eec6537a0b77b8bfbc47de68cc258c5b
hlv.nguyentrinh@gmail.com <hlv.nguyentrinh@gmail.com@515b9638-c7ee-b3bb-69fa-1e28f7a59f5f>
2011-11-30 03:49:43 +0000

    - Fix some Environment variables

--- Project/Project - Irrlicht/VS2010/demo/CStateIngame.cpp
+++ Project/Project - Irrlicht/VS2010/demo/CStateIngame.cpp
@@ -3,7 +3,7 @@
 
 
 
-	
+
 //--------------------------
 
 CStateIngame::CStateIngame():CState()
@@ -18,8 +18,8 @@ void CStateIngame::Init()
 	CGameObject::smgr = smgr;
 	CGameObject::driver = driver;
 	CGameObject::guienv = guienv;	
-	
-    //device->getCursorControl()->setVisible(false);
+
+	//device->getCursorControl()->setVisible(false);
 
 	//TempObject:	2 tower at (5,0) and (8,5), 
 	//				1 enemy at (2,2)
@@ -43,13 +43,13 @@ void CStateIngame::Init()
 	camera->setTarget(center);
 	camera->setFarValue(42000.0f);
 
-	
+
 	irr::scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(map->sceneNode->getTriangleSelector(), camera, irr::core::vector3df(1,10,1),irr::core::vector3df(0,-10000.0f,0));
 	camera->addAnimator(anim);
 	anim->drop();
 
 	//
-	
+
 	irr::core::vector3df lightpos = center+irr::core::vector3df(0,50,0);
 
 	irr::scene::ILightSceneNode* light = smgr->addLightSceneNode(0, lightpos, irr::video::SColorf(1.0f, 1.0f, 1.0f, 1.0f), 800.0f);
@@ -63,11 +63,11 @@ void CStateIngame::Init()
 	light->setLightData(light_data);
 
 	//
-	
+
 	Money_text = guienv->addStaticText(L"Money : -",irr::core::rect<irr::s32>(10,10,110,20),false,false,0,-1,true);
 	Life_text = guienv->addStaticText(L"Lives : -",irr::core::rect<irr::s32>(10,30,110,40),false,false,0,-1,true);
 
-	
+
 }
 
 void CStateIngame::Update()
@@ -79,37 +79,37 @@ void CStateIngame::Update()
 		highlightedSceneNode = 0;
 	}
 	ObjectManager.Update(CFpsController::GetInstance()->GetFrameDt());
-	
+
 	Money_text->setText((irr::core::stringw(L"Money : ")+irr::core::stringw(ObjectManager.Map->Money)).c_str());
 	Life_text->setText((irr::core::stringw(L"Lives : ")+irr::core::stringw(ObjectManager.Map->RemainingLife)).c_str());
 
 	irr::core::line3d<irr::f32> ray;
 	irr::scene::ICameraSceneNode* camera = smgr->getActiveCamera();
-		ray.start = camera->getPosition();
-		ray.end = ray.start + (camera->getTarget() - ray.start).normalize() * 100000.0f;
-		irr::core::vector3df intersection;
-		irr::core::triangle3df hitTriangle;
-		irr::scene::ISceneNode * selectedSceneNode = smgr->getSceneCollisionManager()->getSceneNodeAndCollisionPointFromRay(
-			ray,
-			intersection, // This will be the position of the collision
-			hitTriangle, // This will be the triangle hit in the collision
-			2, // This ensures that only nodes that we have
-					// set up to be pickable are considered
-			0); // Check the entire scene (this is actually the implicit default)
-		if (selectedSceneNode)
-		{
-			driver->setTransform(irr::video::ETS_WORLD, irr::core::matrix4());
-			irr::video::SMaterial material;
-			material.setTexture(0, 0);
-			material.Lighting = false;
-			material.NormalizeNormals = false;
-			material.Wireframe = true;
-			driver->setMaterial(material);
-			driver->draw3DTriangle(hitTriangle, irr::video::SColor(0,255,0,0));
+	ray.start = camera->getPosition();
+	ray.end = ray.start + (camera->getTarget() - ray.start).normalize() * 100000.0f;
+	irr::core::vector3df intersection;
+	irr::core::triangle3df hitTriangle;
+	irr::scene::ISceneNode * selectedSceneNode = smgr->getSceneCollisionManager()->getSceneNodeAndCollisionPointFromRay(
+		ray,
+		intersection, // This will be the position of the collision
+		hitTriangle, // This will be the triangle hit in the collision
+		2, // This ensures that only nodes that we have
+		// set up to be pickable are considered
+		0); // Check the entire scene (this is actually the implicit default)
+	if (selectedSceneNode)
+	{
+		driver->setTransform(irr::video::ETS_WORLD, irr::core::matrix4());
+		irr::video::SMaterial material;
+		material.setTexture(0, 0);
+		material.Lighting = false;
+		material.NormalizeNormals = false;
+		material.Wireframe = true;
+		driver->setMaterial(material);
+		driver->draw3DTriangle(hitTriangle, irr::video::SColor(0,255,0,0));
 
-			highlightedSceneNode = selectedSceneNode;
-			highlightedSceneNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-		}
+		highlightedSceneNode = selectedSceneNode;
+		highlightedSceneNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	}
 
 
 
